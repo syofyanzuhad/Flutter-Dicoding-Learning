8be5ffd880ebd08a857242797d3815f69f04669a
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2021-07-10 09:54:16 +0700

    learn: sample-row-col

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: FirstScreen()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: FirstScreen());
   }
 }
 
@@ -26,7 +25,8 @@ class FirstScreen extends StatelessWidget {
           icon: Icon(
             Icons.menu,
             color: Colors.white,
-          ), onPressed: () {  },
+          ),
+          onPressed: () {},
         ),
         title: Text('First Screen'),
         actions: [
@@ -34,24 +34,25 @@ class FirstScreen extends StatelessWidget {
             icon: Icon(
               Icons.search,
               color: Colors.white,
-            ), onPressed: () {  },
+            ),
+            onPressed: () {},
           ),
         ],
       ),
       body: Center(
-        // child: Text('Hello world!'),
-        // child: Container(
-        //   child: Text('Hi', style: TextStyle(fontSize: 40),),
-        //   decoration: BoxDecoration(
-        //     color: Colors.red,
-        //     border: Border.all(color: Colors.green,width: 3),
-        //     borderRadius: BorderRadius.circular(10),
-        //   ),
-        // )
-        // child: Padding(
-        //   padding: EdgeInsets.all(30),
-        //   child: Text('Ini Padding'),
-        // ),
+          // child: Text('Hello world!'),
+          // child: Container(
+          //   child: Text('Hi', style: TextStyle(fontSize: 40),),
+          //   decoration: BoxDecoration(
+          //     color: Colors.red,
+          //     border: Border.all(color: Colors.green,width: 3),
+          //     borderRadius: BorderRadius.circular(10),
+          //   ),
+          // )
+          // child: Padding(
+          //   padding: EdgeInsets.all(30),
+          //   child: Text('Ini Padding'),
+          // ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           // mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -59,27 +60,83 @@ class FirstScreen extends StatelessWidget {
           // mainAxisAlignment: MainAxisAlignment.start,
           // mainAxisAlignment: MainAxisAlignment.center,
           // mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget> [
-            IconButton(
-              icon: Icon(Icons.share), 
-              onPressed: () {  },
-              color: Colors.grey,
-            ),
-            IconButton(
-              icon: Icon(
-              Icons.thumb_up),
-              onPressed: () {  },
-              color: Colors.blue,
-            ),
-            IconButton(
-              icon: Icon(Icons.thumb_down),
-              onPressed: () {  },
-            ),
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Text(
+                  'Sebuah Judul',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                ),
+                Text('Lorem ipsum dolor sit amet'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: Colors.yellow[600],
+                        ),
+                        Icon(Icons.star,
+                          color: Colors.yellow[600],
+                        ),
+                        Icon(Icons.star,
+                          color: Colors.yellow[600],
+                        ),
+                        Icon(Icons.star,
+                          color: Colors.yellow[600],
+                        ),
+                        Icon(Icons.star,
+                          color: Colors.grey,
+                        ),
+                      ],
+                    ),
+                    Text('170 Reviews')
+                  ],
+                ),
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.book,
+                          color: Colors.green[200],
+                        ),
+                        Text('PREP:'),
+                        Text('25 Min')
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.timer,
+                          color: Colors.green[200],
+                        ),
+                        Text('COOK:'),
+                        Text('1 Hr')
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.restaurant_menu_outlined,
+                          color: Colors.green[200],
+                        ),
+                        Text('FEEDS:'),
+                        Text('2-5 Min')
+                      ],
+                    )
+                  ],
+                ),
+                
+              ],
+            )
           ],
         )
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
+        onPressed: () {},
         child: Icon(Icons.add),
       ),
     );
