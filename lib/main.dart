357b38c3086ed78056934d3885136aa4de32b4a3
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2021-07-16 08:17:46 +0700

    added: responsive-media-query

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Oswald',
           primarySwatch: Colors.blue,
         ),
-        home: FirstScreen());
+        home: HomePage());
   }
 }
 
@@ -39,6 +39,34 @@ class FirstScreen extends StatelessWidget {
             }));
           },
         ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    Orientation orientation = MediaQuery.of(context).orientation;
+ 
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'Screen width: ${screenSize.width.toStringAsFixed(2)}',
+            style: TextStyle(color: Colors.white, fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            'Orientation: $orientation',
+            style: TextStyle(color: Colors.white, fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
