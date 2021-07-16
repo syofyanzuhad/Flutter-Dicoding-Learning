8c0724a5bdda2f364270e6f11415d5d8835d44b3
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2021-07-16 08:28:51 +0700

    added: responsivePage-with-layoutBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Oswald',
           primarySwatch: Colors.blue,
         ),
-        home: HomePage());
+        home: ResponsivePage());
   }
 }
 
@@ -41,6 +41,44 @@ class FirstScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class ResponsivePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth < 600) {
+            return ListView(
+              children: _generateContainers(),
+            );
+          } else if (constraints.maxWidth < 900) {
+            return GridView.count(
+              crossAxisCount: 2,
+              children: _generateContainers(),
+            );
+          } else {
+            return GridView.count(
+              crossAxisCount: 6,
+              children: _generateContainers(),
+            );
+          }
+        },
+      ),
+    );
+  }
+ 
+  List<Widget> _generateContainers() {
+    return List<Widget>.generate(20, (index) {
+      return Container(
+        margin: const EdgeInsets.all(8),
+        color: Colors.blueGrey,
+        height: 200,
+      );
+    });
   }
 }
 
